12f6fa01f3103323264cd62c3e159003949fa4e9
Alexander Usmanov <ausmanov@igsystems.ru>
2016-05-11 17:47:01 +0300

    * парсинг правил из строк

--- FuzzyLogic.Mamdani/Rule.cs
+++ FuzzyLogic.Mamdani/Rule.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FuzzyLogic.Mamdani.Statements;
 
 namespace FuzzyLogic.Mamdani
@@ -24,6 +26,26 @@ namespace FuzzyLogic.Mamdani
             list.Add(Conclusion.ToString());
 
             return list.ToArray();
+        }
+
+        public static Rule Create(string input, List<LingVariable> variables)
+        {
+            var splitResult = input.Split(new[] { " => " }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (splitResult.Length < 2)
+                throw new ArgumentException("не найден разделитель => ", nameof(input));
+
+            var conditionsStrings = splitResult[0].Split(new[] { " & " }, StringSplitOptions.RemoveEmptyEntries);
+            var conditions = conditionsStrings.Select(x => Condition.Create(x, variables));
+
+            var conclusion = Conclusion.Create(splitResult[1], variables);
+
+            var statementsCount = conditions.Count() + 1;
+
+            if(statementsCount != variables.Count)
+                throw new ArgumentException($"некорректная входная строка, число выражений {statementsCount} не равно числу переменных {variables.Count}", nameof(input));
+
+            return new Rule(conditions.ToList(), conclusion);
         }
     }
 }
--- FuzzyLogic.Mamdani/Statements/Condition.cs
+++ FuzzyLogic.Mamdani/Statements/Condition.cs
@@ -1,4 +1,8 @@
-﻿namespace FuzzyLogic.Mamdani.Statements
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuzzyLogic.Mamdani.Statements
 {
     /// <summary>
     /// Условие
@@ -8,6 +12,24 @@
         public Condition(LingVariable variable, Term term)
             : base(variable, term)
         {
+        }
+
+        public static Condition Create(string input, List<LingVariable> variables)
+        {
+            var splitResult = input.Split('=');
+            if (splitResult.Length < 2)
+                throw new ArgumentException("не найден симовл = в выражении", nameof(input));
+
+            var variable = variables.FirstOrDefault(x => x.Name == splitResult[0]);
+            var term = variable?.Terms.FirstOrDefault(x => x.Name == splitResult[1]);
+
+            if (variable == null)
+                throw new ArgumentException($"не найдена переменная {splitResult[0]}", nameof(input));
+
+            if (term == null)
+                throw new ArgumentException($"не найдено значение {splitResult[1]} переменной {splitResult[0]}", nameof(input));
+
+            return new Condition(variable, term);
         }
     }
 }
